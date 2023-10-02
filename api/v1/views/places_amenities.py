e77cfdefc5ff1f8952d99f548e7773056176094a
Modeste Ciira <ciiramodeste23@gmail.com>
2023-10-02 16:32:08 +0300

    changes made and new api method updated

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from flask import jsonify, abort
+from api.v1.views import app_views
+from models.place import Place
+from models.amenity import Amenity
+from models import storage
+
+
+@app_views.route('/places/<place_id>/amenities', strict_slashes=False)
+def get_place_amenities(place_id):
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities = [a.to_dict() for a in place.amenities]
+    return jsonify(amenities)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None or amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def link_amenity_to_place(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None or amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenites.append(amenity)
+    storage.save()
+    return jsonify(amenity.to_dict())
