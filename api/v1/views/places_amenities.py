4d162ec2552b47402d209c738523b12e9e89d046
Modeste Ciira <ciiramodeste23@gmail.com>
2023-10-02 17:11:29 +0300

    changes made

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -36,8 +36,8 @@ def link_amenity_to_place(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if place is None or amenity is None:
         abort(404)
-    if amenity not in place.amenities:
-        abort(404)
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
     place.amenites.append(amenity)
     storage.save()
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity.to_dict()), 201
